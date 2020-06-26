1e11e669a13475b5de9ee4b6c1d11333b31c55cd
IAlexstrazaI <nekker1337@gmail.com>
2020-06-26 12:08:13 +0300

    Написана функция ввода матрицы

---
+++ Praktika.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define N 5
+
+void input_matr(int a[N][N]) {
+	int i, j;
+	printf("Введите элементы матрицы a: \n");
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			printf("a[%d][%d]:", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+}
