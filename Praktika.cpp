cd9496d80e02ceb0b81984b0c6207fbea5fee03a
IAlexstrazaI <nekker1337@gmail.com>
2020-06-30 11:21:00 +0300

    Dopisana onovnaya chast' programmy i fukcii obrabotki i vyvoda matricy, odnako iz-za osobennosti Visual Studio prishlos' dobavit' ignorirovanie nekotoryh oshibok v proekt: #define _CRT_SECURE_NO_WARNINGS

--- Praktika.cpp
+++ Praktika.cpp
@@ -1,10 +1,9 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 #include <locale.h>
 #define N 5
-
 void input_matr(int a[N][N]) {
 	int i, j;
 	printf("Введите элементы матрицы a: \n");
@@ -39,4 +38,77 @@ int minMassiva(int x[N], int const arraySize, int min, int i)
 	else {
 		return min;
 	}
+}
+
+int* mas_x(int(*a)[N], int* x, void(*pfunc)(int[N][N])) {
+	int i, j, max;
+	pfunc(a);
+	for (i = 0; i < N; i++) {
+		max = a[i][0];
+		for (j = 0; j < N; j++) {
+			if (a[i][j] >= max) {
+				x[i] = a[i][j];
+				max = a[i][j];
+			}
+		}
+	}
+	printf("min = %d", minMassiva(x, N, x[0], 1));
+	printf("\n");
+	return x;
+}
+
+void output(const int x[], const int a[N][N])
+{
+	int i, j;
+	printf("Матрица А:\n");
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+			printf("%5d", a[i][j]);
+		printf("\n");
+	}
+	printf("Массив Х:\n");
+	for (i = 0; i < N; i++)
+		printf("%5d", x[i]);
+	printf("\n");
+}
+
+void output2(const int x[], const int a[N][N]) {
+	int i, j;
+	FILE* fp;
+	fp = fopen("Lab72.txt", "w");
+	if (fp) {
+		fprintf(fp, "Матрица А:\n");
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++)
+				fprintf(fp, "%5d", a[i][j]);
+			fprintf(fp, "\n");
+
+		}
+		fprintf(fp, "Массив Х:\n");
+		for (i = 0; i < N; i++)
+			fprintf(fp, "%5d", x[i]);
+		fprintf(fp, "\n");
+		fclose(fp);
+	}
+}
+
+int main() {
+	setlocale(LC_CTYPE, "");
+	int a[N][N], x[N], c;
+	void(*pfunc)(int[N][N]);
+	do
+	{
+		printf("Ввод матрицы:\n1 - с клавиатуры\n2- Из файла\n");
+		scanf("%d", &c);
+	} while (c != 1 && c != 2);
+	switch (c)
+	{
+	case 1: pfunc = &input_matr;
+		break;
+	case 2: pfunc = &input_matr2;
+	}
+	output(mas_x(a, x, pfunc), a);
+	output2(mas_x(a, x, pfunc), a);
+	_getch();
 }
